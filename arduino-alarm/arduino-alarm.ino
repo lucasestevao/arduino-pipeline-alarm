3c6956bc7764cf875cf5c35511ae594e54d309f2
Lucas Estevão <lucasestevaods@gmail.com>
2019-05-10 11:30:43 -0300

    Adding connection to wifi.

---
+++ arduino-alarm/arduino-alarm.ino
@@ -0,0 +1,38 @@
+#include <ESP8266WiFi.h>
+
+/* setup wifi - begin */
+const char *ssid = "mywifissid";
+const char *password = "mypassword";
+/* setup wifi - end */
+
+
+void setup()
+{
+  Serial.begin(115200); // Start the Serial communication to send messages to the computer
+  delay(10);
+  Serial.println("\n");
+
+  WiFi.begin(ssid, password); // Connect to the network
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+  Serial.println(" ...");
+
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED)
+  { // Wait for the Wi-Fi to connect
+    delay(1000);
+    Serial.print(++i);
+    Serial.print(" ");
+  }
+
+  Serial.println("\n");
+  Serial.println("Connection established!");
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());
+}
+
+void loop()
+{
+  Serial.print("\nRunning loop \n");
+  delay(1000);
+}
